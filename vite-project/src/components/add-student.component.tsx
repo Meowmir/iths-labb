051d4ebb9391e1ce7035bf701b5bf0e2208122be
Meowmir <disewji@gmail.com>
2024-03-21 14:36:21 +0100

    add separate component for textfield

--- vite-project/src/components/add-student.component.tsx
+++ vite-project/src/components/add-student.component.tsx
@@ -1,30 +1,12 @@
-import {Box, Button, TextField} from "@mui/material";
+import {Box, Button} from "@mui/material";
+import NameTextfield from "./name-textfield.component.tsx";
 
 export default function AddStudent({onForenameChange, onSurnameChange, onClick} : {onForenameChange: (forename: string) => void, onSurnameChange: (surname: string) => void, onClick: () => void}){
   return (
     <>
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-        <TextField
-          label="Forename"
-          variant="outlined"
-          color="primary"
-          sx={{
-            backgroundColor: "white",
-            borderRadius: 2,
-          }}
-          onChange={(forename) => onForenameChange(forename.target.value)}
-        />
-        <TextField
-          label="Surname"
-          variant="outlined"
-          color="primary"
-          sx={{
-            backgroundColor: "white",
-            borderRadius: 2,
-            marginLeft: 1
-          }}
-          onChange={(surname) => onSurnameChange(surname.target.value)}
-        />
+        <NameTextfield onChange={onForenameChange} name={"Forename"}/>
+        <NameTextfield onChange={onSurnameChange} name={"Surname"}/>
         <Button
           variant="contained"
           sx={{ marginLeft: 2}}
